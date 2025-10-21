a79b623413b709fd11ae5c73cb12aad5a102988f
K-Yulia <109465660+K-Yulia@users.noreply.github.com>
2025-10-21 14:51:23 +0300

    fix bug

--- js/menu.js
+++ js/menu.js
@@ -3,23 +3,47 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    header: document.querySelector(".page-header"),
     navList: document.querySelector(".js-nav"),
+    backdrop: document.querySelector("[data-menu-backdrop]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleModal);
-  refs.closeMenuBtn.addEventListener("click", toggleModal);
-  refs.navList.addEventListener("click", onListClick);
+  let scrollPosition = 0;
 
-  function toggleModal() {
-    nav = refs.menu.classList.toggle("is-open");
-    if (nav) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "scroll";
+  const openMenu = () => {
+    scrollPosition = window.scrollY;
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
+    refs.menu.classList.add("is-open");
+    refs.header.classList.add("menu-open");
+  };
+
+  const closeMenu = () => {
+    refs.menu.classList.remove("is-open");
+    refs.header.classList.remove("menu-open");
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    window.scrollTo(0, scrollPosition);
+  };
+
+  refs.openMenuBtn.addEventListener("click", openMenu);
+  refs.closeMenuBtn.addEventListener("click", closeMenu);
+
+  refs.navList.addEventListener("click", (event) => {
+    if (event.target.nodeName === "A") {
+      closeMenu();
     }
-  }
-})();
+  });
 
-function onListClick(ev) {
-  refs.burgerMenu.classList.remove("is-open");
-}
+  refs.backdrop.addEventListener("click", closeMenu);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 70) {
+      refs.header.classList.add("scrolled");
+    } else {
+      refs.header.classList.remove("scrolled");
+    }
+  });
+})();
